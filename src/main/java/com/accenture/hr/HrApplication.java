af89e4778206ea4b927ce4a085ca44fa0e4a1e9b
pocz.mate <mate.pocz@gmail.com>
2020-06-25 14:14:00 +0200

    add currentSafetyDistance

--- src/main/java/com/accenture/hr/HrApplication.java
+++ src/main/java/com/accenture/hr/HrApplication.java
@@ -15,6 +15,8 @@ public class HrApplication {
     private int maxCapacity;
     @Value("${currentAllowancePercent}")
     private int currentAllowancePercent;
+    @Value("${currentSafetyDistance}")
+    private int currentSafetyDistance;
 
     public static void main(String[] args) {
         SpringApplication.run(HrApplication.class, args);
@@ -33,5 +35,29 @@ public class HrApplication {
     @Bean
     public Integer currentLimit() {
         return (int) (maxCapacity * ((double) currentAllowancePercent / 100));
+    }
+
+    @Bean
+    public String currentSafetyDistance() {
+        String fileName = "src/main/resources/coordinates/";
+        switch (currentSafetyDistance) {
+            case 5:
+                fileName += "coordinates_five_meters.txt";
+                break;
+            case 4:
+                fileName += "coordinates_four_meters.txt";
+                break;
+            case 3:
+                fileName += "coordinates_three_meters.txt";
+                break;
+            case 2:
+                fileName += "coordinates_two_meters.txt";
+                break;
+            case 1:
+                fileName += "coordinates_one_meter.txt";
+                break;
+            default:
+        }
+        return fileName;
     }
 }

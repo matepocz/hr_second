83dd9b0f9085d8a875fd05d94b4a44ef3f8aa93f
pocz.mate <mate.pocz@gmail.com>
2020-06-25 11:40:39 +0200

    impl WorkSpace

---
+++ src/main/java/com/accenture/hr/model/WorkSpace.java
@@ -0,0 +1,16 @@
+package com.accenture.hr.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.awt.*;
+
+@AllArgsConstructor
+@Getter
+public class WorkSpace {
+
+    private int x;
+    private int y;
+    private Color color;
+
+}

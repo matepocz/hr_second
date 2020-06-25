9d1bb29a72ad9cd24522d9953679c14065768bcf
pocz.mate <mate.pocz@gmail.com>
2020-06-25 14:15:51 +0200

    add CoordinateService

---
+++ src/main/java/com/accenture/hr/service/CoordinateService.java
@@ -0,0 +1,43 @@
+package com.accenture.hr.service;
+
+import com.accenture.hr.model.WorkSpace;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CoordinateService {
+
+    private final String fileName;
+
+    @Autowired
+    public CoordinateService(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public List<WorkSpace> getAllowedWorkSpaces() {
+        List<WorkSpace> allowedWorkSpaces = new ArrayList<>();
+        Path filePath = Paths.get(fileName);
+        try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+            String line;
+            while ((line = reader.readLine()) != null){
+                String[] coords = line.split(",");
+                int x = Integer.parseInt(coords[0]);
+                int y = Integer.parseInt(coords[1]);
+                WorkSpace workSpace = new WorkSpace(x, y);
+                allowedWorkSpaces.add(workSpace);
+            }
+        } catch (NumberFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return allowedWorkSpaces;
+    }
+}

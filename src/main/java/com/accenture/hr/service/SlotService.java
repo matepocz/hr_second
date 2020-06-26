dfa81e88e790d45e013db52261fff8330231315d
Bakoczy Zoltan <bakoczy.zoltan@gmail.com>
2020-06-26 14:14:34 +0200

    empty commit

--- src/main/java/com/accenture/hr/service/SlotService.java
+++ src/main/java/com/accenture/hr/service/SlotService.java
@@ -31,7 +31,6 @@ public class SlotService {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-
     @Autowired
     public SlotService(Integer currentLimit, List<Long> peopleInside, List<Long> peopleWaiting,
                        List<Long> vipPersons) {

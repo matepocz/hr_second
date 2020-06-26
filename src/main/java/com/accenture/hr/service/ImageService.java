1f03ab62f38a6a16383527452f330d547b421583
pocz.mate <mate.pocz@gmail.com>
2020-06-27 00:33:03 +0200

    add currentSafetyDistance field

--- src/main/java/com/accenture/hr/service/ImageService.java
+++ src/main/java/com/accenture/hr/service/ImageService.java
@@ -3,7 +3,6 @@ package com.accenture.hr.service;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.process.ImageProcessor;
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
@@ -14,24 +13,26 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 
 public class ImageService {
     private static final Logger log = LoggerFactory.getLogger(ImageService.class);
 
+    private final int currentSafetyDistance = 1;
+
     private static final String ABS_CURRENT_LAYOUT = "/home/student/mentoring/students/accenture-contest/src/main/resources/images/office_layout.jpg";
     private static final String CURRENT_LAYOUT = "images/office_layout.jpg";
-    private static final String TEMP_LAYOUT = "src/main/resources/test.jpg";
+    private static final String TEMP_LAYOUT = "src/main/resources/images/temp_layout.jpg";
 
     public void drawWorkSpace(int x, int y, Color color) {
+        int radiusOfCircle = currentSafetyDistance * 10;
+        int circlesOffset = (int) Math.ceil(radiusOfCircle / (double) 2);
         String tempFilePath = getImgFile(CURRENT_LAYOUT);
-
         ImagePlus imagePlus = IJ.openImage(tempFilePath);
         ImageProcessor ip = imagePlus.getProcessor();
         ip.setColor(color);
         ip.drawOval(x - 3, y - 3, 6, 6);
-        ip.drawOval(x - 10, y - 10, 20, 20);
+        ip.drawOval(x - circlesOffset, y - circlesOffset, radiusOfCircle, radiusOfCircle);
         log.debug(String.format("Workspace colored to %s", color));
 
         BufferedImage newImg = imagePlus.getBufferedImage();

79c4873148a5f92f42465abbbf4779605bc9ef4b
pocz.mate <mate.pocz@gmail.com>
2020-06-27 23:50:12 +0200

    add URL field

--- src/main/java/com/accenture/hr/responses/EntryResponse.java
+++ src/main/java/com/accenture/hr/responses/EntryResponse.java
@@ -2,9 +2,12 @@ package com.accenture.hr.responses;
 
 import com.accenture.hr.enums.StatusList;
 
+import java.net.URL;
+
 public class EntryResponse {
 
     private StatusList status;
+    private URL url;
 
     public StatusList getStatus() {
         return status;
@@ -12,5 +15,13 @@ public class EntryResponse {
 
     public void setStatus(StatusList status) {
         this.status = status;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
     }
 }

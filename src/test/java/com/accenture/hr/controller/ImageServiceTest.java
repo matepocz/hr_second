a2c5018618e048da69aa3be9c3f1ce04253cce62
pocz.mate <mate.pocz@gmail.com>
2020-06-27 21:11:33 +0200

    add argument

--- src/test/java/com/accenture/hr/controller/ImageServiceTest.java
+++ src/test/java/com/accenture/hr/controller/ImageServiceTest.java
@@ -2,7 +2,6 @@ package com.accenture.hr.controller;
 
 import com.accenture.hr.service.ImageService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.awt.*;
@@ -10,13 +9,13 @@ import java.awt.*;
 @SpringBootTest
 public class ImageServiceTest {
 
-    private ImageService imageService = new ImageService();
+    private ImageService imageService = new ImageService(1);
 
     @Test
     public void testImgEditing() {
 
-        imageService.drawWorkSpace(715,100, Color.BLACK);
-       // imageService.testImgEditing(715, 100, 20, 10, Color.RED);
+        imageService.drawWorkSpace(715, 100, Color.BLACK);
+        // imageService.testImgEditing(715, 100, 20, 10, Color.RED);
         // imageService.testImgEditing(765, 100, 20, 10, Color.BLACK);
     }
 }
